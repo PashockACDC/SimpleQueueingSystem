098f9b4688466ac3d4fcd60097cbcca6f1badb59
Pashock <pashockacdc@mail.ru>
2016-09-03 00:35:31 +0300

    try # 1

--- src/main/java/com/example/pashock/simplequeueingsystem/MainActivity.java
+++ src/main/java/com/example/pashock/simplequeueingsystem/MainActivity.java
@@ -2,14 +2,24 @@ package com.example.pashock.simplequeueingsystem;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TableLayout;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements View.OnClickListener {
+
+    TableLayout tl;
 
     /** Called when the activity is first created. */
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        tl = new TableLayout(this);
+
+    }
+
+    @Override
+    public void onClick(View view) {
 
     }
 }
