2e0e3fe8025759153bc617fe4b30f93f0877b770
Pashock <pashockacdc@mail.ru>
2016-09-03 23:20:07 +0300

    try to add button in TableLayout programmatically

--- src/main/java/com/example/pashock/simplequeueingsystem/MainActivity.java
+++ src/main/java/com/example/pashock/simplequeueingsystem/MainActivity.java
@@ -3,23 +3,41 @@ package com.example.pashock.simplequeueingsystem;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.RelativeLayout;
 import android.widget.TableLayout;
 
 public class MainActivity extends Activity implements View.OnClickListener {
 
-    TableLayout tl;
+    static int n = 1;
+    Button btn1;
+    TableLayout tl1;
 
     /** Called when the activity is first created. */
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        tl = new TableLayout(this);
+        tl1 = (TableLayout)findViewById(R.id.tl1);
+        btn1 = (Button)findViewById(R.id.btn1);
 
+        btn1.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View view) {
+        switch (view.getId()) {
 
+            case R.id.btn1:
+                Button btn = new Button(this);
+                RelativeLayout.LayoutParams layoutParams = (RelativeLayout.LayoutParams)btn1.getLayoutParams();
+                
+                btn.setLayoutParams(layoutParams);
+                //btn.setText(n++);
+
+                tl1.addView(btn);
+                break;
+
+        }
     }
 }
