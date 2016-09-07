90e6e2bd291975e6dd2a20e2f543220c1f0e79fd
PashockACDC <pashockacdc@mail.ru>
2016-09-08 00:30:34 +0300

    try to adding rows into TableLyaouts

--- src/main/java/com/example/pashock/simplequeueingsystem/MainActivity.java
+++ src/main/java/com/example/pashock/simplequeueingsystem/MainActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.TableLayout;
 import android.widget.TableRow;
@@ -11,10 +12,11 @@ import android.widget.TextView;
 
 public class MainActivity extends Activity implements View.OnClickListener {
 
-    static int n = 1;
+    static int n = 100;
     //TableLayout tl1;
     TableLayout[] tl;
     TableRow[] tableRow;
+    ImageView[] imageViews;
 
     /** Called when the activity is first created. */
     public void onCreate(Bundle savedInstanceState) {
@@ -23,9 +25,11 @@ public class MainActivity extends Activity implements View.OnClickListener {
 
         tl = new TableLayout[4];
         tableRow = new TableRow[4];
+        imageViews =  new ImageView[4];
         for (int i = 0; i < 4; i++) {
             tl[i] = new TableLayout(this);
             tableRow[i] = new TableRow(this);
+            imageViews[i] = new ImageView(this);
         }
         tl[0] = (TableLayout)findViewById(R.id.tl1);
         tl[1] = (TableLayout)findViewById(R.id.tl2);
@@ -35,37 +39,43 @@ public class MainActivity extends Activity implements View.OnClickListener {
         tableRow[1] = (TableRow)findViewById(R.id.tr2);
         tableRow[2] = (TableRow)findViewById(R.id.tr3);
         tableRow[3] = (TableRow)findViewById(R.id.tr4);
+        imageViews[0] = (ImageView)findViewById(R.id.image_Device_1);
+        imageViews[1] = (ImageView)findViewById(R.id.image_Device_2);
+        imageViews[2] = (ImageView)findViewById(R.id.image_Device_3);
+        imageViews[3] = (ImageView)findViewById(R.id.image_Device_4);
 
         for (int i = 0; i < 4; i++)
-            tl[i].setOnClickListener(this);
+            imageViews[i].setOnClickListener(this);
     }
 
     @Override
     public void onClick(View view) {
         switch (view.getId()) {
 
-            case R.id.tl1:
-            case R.id.tl2:
-            case R.id.tl3:
-            case R.id.tl4:
+            case R.id.image_Device_1:
+            case R.id.image_Device_2:
+            case R.id.image_Device_3:
+            case R.id.image_Device_4:
                 TextView tv = new TextView(this);
                 TableLayout.LayoutParams layoutParams = (TableLayout.LayoutParams)tableRow[0].getLayoutParams();
 
                 tv.setLayoutParams(layoutParams);
                 tv.setText("request # " + String.valueOf(n++));
-                tv.setTextSize(10/*R.dimen.regular_text_in_table*/);
+                float textSize = this.getResources().getDimension(R.dimen.regular_text_in_table);
+                tv.setTextSize(textSize);
+                tv.setVisibility(View.VISIBLE);
 
                 switch (view.getId()) {
-                    case R.id.tl1:
+                    case R.id.image_Device_1:
                         tl[0].addView(tv);
                         break;
-                    case R.id.tl2:
+                    case R.id.image_Device_2:
                         tl[1].addView(tv);
                         break;
-                    case R.id.tl3:
+                    case R.id.image_Device_3:
                         tl[2].addView(tv);
                         break;
-                    case R.id.tl4:
+                    case R.id.image_Device_4:
                         tl[3].addView(tv);
                         break;
                 }
