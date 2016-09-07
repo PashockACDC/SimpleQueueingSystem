cb45b6eb604432539cd28eea94fb4f8dac505988
PashockACDC <pashockacdc@mail.ru>
2016-09-08 02:53:28 +0300

    V.0.2 (#2)
    * small fix
    
    * made test rows invisible

--- src/main/java/com/example/pashock/simplequeueingsystem/MainActivity.java
+++ src/main/java/com/example/pashock/simplequeueingsystem/MainActivity.java
@@ -3,19 +3,16 @@ package com.example.pashock.simplequeueingsystem;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.ImageView;
-import android.widget.RelativeLayout;
 import android.widget.TableLayout;
 import android.widget.TableRow;
 import android.widget.TextView;
 
 public class MainActivity extends Activity implements View.OnClickListener {
 
-    static int n = 100;
-    //TableLayout tl1;
+    static int n = 1;
     TableLayout[] tl;
-    TableRow[] tableRow;
+    TableRow tableRow;
     ImageView[] imageViews;
 
     /** Called when the activity is first created. */
@@ -24,21 +21,16 @@ public class MainActivity extends Activity implements View.OnClickListener {
         setContentView(R.layout.activity_main);
 
         tl = new TableLayout[4];
-        tableRow = new TableRow[4];
         imageViews =  new ImageView[4];
         for (int i = 0; i < 4; i++) {
             tl[i] = new TableLayout(this);
-            tableRow[i] = new TableRow(this);
             imageViews[i] = new ImageView(this);
         }
         tl[0] = (TableLayout)findViewById(R.id.tl1);
         tl[1] = (TableLayout)findViewById(R.id.tl2);
         tl[2] = (TableLayout)findViewById(R.id.tl3);
         tl[3] = (TableLayout)findViewById(R.id.tl4);
-        tableRow[0] = (TableRow)findViewById(R.id.tr1);
-        tableRow[1] = (TableRow)findViewById(R.id.tr2);
-        tableRow[2] = (TableRow)findViewById(R.id.tr3);
-        tableRow[3] = (TableRow)findViewById(R.id.tr4);
+        tableRow = (TableRow)findViewById(R.id.tr1);
         imageViews[0] = (ImageView)findViewById(R.id.image_Device_1);
         imageViews[1] = (ImageView)findViewById(R.id.image_Device_2);
         imageViews[2] = (ImageView)findViewById(R.id.image_Device_3);
@@ -57,7 +49,7 @@ public class MainActivity extends Activity implements View.OnClickListener {
             case R.id.image_Device_3:
             case R.id.image_Device_4:
                 TextView tv = new TextView(this);
-                TableLayout.LayoutParams layoutParams = (TableLayout.LayoutParams)tableRow[0].getLayoutParams();
+                TableLayout.LayoutParams layoutParams = (TableLayout.LayoutParams)tableRow.getLayoutParams();
 
                 tv.setLayoutParams(layoutParams);
                 tv.setText("request # " + String.valueOf(n++));
@@ -79,7 +71,6 @@ public class MainActivity extends Activity implements View.OnClickListener {
                         tl[3].addView(tv);
                         break;
                 }
-                //tl1.addView(tv);
                 break;
 
         }
